ee713bf27aaea5fe984f9603aa6a02677ebc50b9
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2011-08-21 13:14:55 +0200

    add urls

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 ?>
 http://www.holatdd.com/videos/point-of-sale
+https://github.com/sebastianbergmann/phpunit/
+https://github.com/hafriedlander/phockito
